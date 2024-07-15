7dedbc8b43316062f79deda83ff5b07f40f07f4a
Amolly <amollychoudhary@gmail.com>
2024-07-15 16:14:37 +0530

    Created basic layout

---
+++ PixPlot/PixPlotWeb/DisplayStyles/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DisplaystylesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'DisplayStyles'
--- PixPlot/pixplot/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PixplotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pixplot'
